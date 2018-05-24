4d87e38a2b2a5ea0217b992e60d88beaadf5f10f
Evan Czyzycki <eczyzycki@narrativescience.com>
2018-05-24 13:31:39 -0500

    backup and temp locations

--- customization.el
+++ customization.el
@@ -1,1 +1,8 @@
+;; load solarized theme
 (load-theme 'solarized-light t)
+
+;; change backup and temporary file locations
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
