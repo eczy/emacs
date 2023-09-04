8dfcb00e1a18dc987390e53c53d10a14edb8ec07
Evan Czyzycki <eczy3826@gmail.com>
2023-09-03 22:11:17 -0600

    add global org-mode bindings

--- org-mode.el
+++ org-mode.el
@@ -1,3 +1,4 @@
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
