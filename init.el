d2cfa34a6ccdf0ce141a09b47087113060a27dc2
Evan Czyzycki <eczy3826@gmail.com>
2023-09-04 23:11:08 -0600

    move custom its own file

--- init.el
+++ init.el
@@ -4,3 +4,6 @@
 (load "~/.emacs.d/plugin-config.el")
 (load "~/.emacs.d/customization.el")
 (load "~/.emacs.d/org-mode.el")
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
