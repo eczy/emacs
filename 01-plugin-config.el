872eb1be7ec7b7ef87f16533ecd9992c343c7bb1
Evan Czyzycki <eczy3826@gmail.com>
2024-04-30 13:14:17 -0600

    disable evil mode in dired

--- 01-plugin-config.el
+++ 01-plugin-config.el
@@ -1,2 +1,5 @@
 ;; start evil mode
 (evil-mode t)
+
+;; evil mode off by default in dired
+(evil-set-initial-state 'dired-mode 'emacs)
--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(company dired-sidebar evil magit modus-themes package-lint
+	     terraform-mode treesit-auto yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
