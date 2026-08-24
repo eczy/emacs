d72aeb0d730393502cab51d932d9ee3da6339e25
Evan Czyzycki <eczy3826@gmail.com>
2026-08-24 15:22:24 -0600

    reorg

--- init.el
+++ init.el
@@ -42,19 +42,6 @@
 
 ;; Enable Evil
 (require 'evil)
-(evil-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;;; --- Org Mode ---
 (global-set-key (kbd "C-c l") #'org-store-link)
@@ -79,3 +66,17 @@
 (add-hook 'rust-mode-hook
           (lambda () (prettify-symbols-mode)))
 
+;;; --- Custom ---
+(evil-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
